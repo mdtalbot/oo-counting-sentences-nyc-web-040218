8fe8560fbfb8a8a3bb18c6ef9cbe64571b8d64bb
Matthew Talbot <matthewtalbot@Matthews-MacBook-Pro.local>
2018-04-06 16:33:48 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "." ? true : false
   end
 
   def question?
-
+    self[-1] == "?" ? true : false
   end
 
   def exclamation?
-
+    self[-1] == "!" ? true : false
   end
 
   def count_sentences
-
+    self.split(/[.!?] /).length
   end
 end
